777976219275058e9b7d890e1faa4d76a9c1a7c8
Strr1t <larionoff2004@gmail.com>
2024-06-13 17:16:40 +0300

    Добавлен метод remove

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,11 +12,11 @@ app.listen(PORT, () => {
   console.log(`Сервер запущен: http://localhost:${PORT}`);
 });
 
-app.get('/list', (req, res) => {
+app.post('/list', (req, res) => {
   res.json(list);
 });
 
-app.get('/add', (req, res) => {
+app.post('/add', (req, res) => {
   const todo = req.body as ITodo;
   const lastTodo = list[list.length - 1];
   const lastId = lastTodo?.id || 0;
@@ -24,5 +24,16 @@ app.get('/add', (req, res) => {
   todo.id = lastId + 1;
 
   list.push(todo);
-  res.json(todo.id);
+  res.json(todo);
+});
+
+app.post('/remove', (req, res) => {
+  const {id} = req.body;
+  const index = list.findIndex((todo) => todo.id === id);
+
+  if(index !== -1){
+    list.splice(index, 1);
+  }
+
+  res.json(index);
 });
