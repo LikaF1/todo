d9a51ad9b206c5b3d048a8a23435cf83c6af7079
Фурьяк Анжелика стажер <afuryak@gmail.com>
2024-06-17 10:31:17 +0000

    Создала модуль, добавила сообщения пользователю, дизоктивировала кнопку без изменений

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo.component';
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -1,7 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { ITodo } from './todo.interface';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class HttpService {
+
+//   constructor(private http: HttpClient) {}
+
+//   getList(): Observable<ITodo[]> {
+//     return this.http.post<ITodo[]>('/api/list', undefined);
+//   }
+
+//   saveTodo(todo: ITodo): Observable<ITodo> {
+//     return this.http.post<ITodo>('/api/add', todo);
+//   }
+
+//   removeTodo(id: number): Observable<ITodo> {
+//     return this.http.post<ITodo>('/api/list', {id});
+//   }
+// }
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ITodo } from './todo.interface';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +36,34 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   getList(): Observable<ITodo[]> {
-    return this.http.post<ITodo[]>('/api/list', undefined);
+    return this.http.post<ITodo[]>('/api/list', undefined).pipe(
+      catchError((error) => this.handleError(error))
+    );
   }
 
   saveTodo(todo: ITodo): Observable<ITodo> {
-    return this.http.post<ITodo>('/api/add', todo);
+    return this.http.post<ITodo>('/api/add', todo).pipe(
+      catchError((error) => this.handleError(error))
+    );
   }
 
-  removeTodo(id: number): Observable<ITodo> {
-    return this.http.post<ITodo>('/api/list', {id});
+  removeTodo(id: number): Observable<any> {
+    return this.http.post<any>('/api/remove', { id }).pipe(
+      catchError((error) => this.handleError(error))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Unknown error occurred!';
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    console.error(errorMessage);
+
+    return throwError(() => new Error(errorMessage));
   }
 }
