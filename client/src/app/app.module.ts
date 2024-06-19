1a8ba7b2412cc7ef8a2e962aa4e2e9147abeb128
Strr1t <larionoff2004@gmail.com>
2024-06-19 10:10:27 +0300

    исправил запятые

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo.component';
 import { HttpService } from './http.service';
-import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,12 +14,12 @@ import { HttpClientModule } from '@angular/common/http';
     TodoComponent,
   ],
   imports: [
-    HttpClientModule,
+    // HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
   ],
   providers: [HttpService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
